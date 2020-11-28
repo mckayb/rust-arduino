dc1c9a48d471f6cff498a841d6eda811e5218305
McKay Broderick <mckay.db@gmail.com>
2020-11-28 12:03:09 -0700

    Did some project renaming

--- rust-arduino-01-blink/flash.sh
+++ rust-arduino-01-blink/flash.sh
@@ -4,7 +4,6 @@ set -e
 
 cargo build
 # avrdude -q -C/etc/avrdude.conf -patmega328p -carduino -P/dev/tty.usbmodem141201 -D "-Uflash:w:$1:e"
-avrdude -q -patmega328p -carduino -P/dev/tty.usbmodem144201 -D -Uflash:w:"target/avr-atmega328p/debug/rust-arduino-spaceship-interface.elf"
-
+avrdude -q -patmega328p -carduino -P/dev/tty.usbmodem144201 -D -Uflash:w:"target/avr-atmega328p/debug/rust-arduino-01-blink.elf"
 
 
