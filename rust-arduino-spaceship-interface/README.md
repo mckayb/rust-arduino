89b0bb2c0e14b83f56380ea2ad42b7f12ba1428f
McKay Broderick <mckay.db@gmail.com>
2020-11-28 11:33:10 -0700

    Cleanup. Added a pic of the wiring diagram needed to make the spaceship interface work

--- rust-arduino-spaceship-interface/README.md
+++ rust-arduino-spaceship-interface/README.md
@@ -1,6 +1,8 @@
 ### Getting Started
 
 ### On a Mac
+1) Wire up the arduino as per the diagram here: [Wiring Diagram](/assets/wiring-diagram.png)
+
 1) Plug in the arduino. (Only tested on an old arduino uno)
 
 2) Build the program and flash it to the arduino
--- rust-arduino-spaceship-interface/flash.sh
+++ rust-arduino-spaceship-interface/flash.sh
@@ -2,16 +2,6 @@
 
 set -e
 
-# if [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
-    # echo "usage: $0 <path-to-binary.elf>" >&2
-    # exit 1
-# fi
-
-# if [ "$#" -lt 1 ]; then
-    # echo "$0: Expecting a .elf file" >&2
-    # exit 1
-# fi
-
 cargo build
 # avrdude -q -C/etc/avrdude.conf -patmega328p -carduino -P/dev/tty.usbmodem141201 -D "-Uflash:w:$1:e"
 avrdude -q -patmega328p -carduino -P/dev/tty.usbmodem144201 -D -Uflash:w:"target/avr-atmega328p/debug/rust-arduino-spaceship-interface.elf"
