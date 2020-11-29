700bd64eea1ab7a77d2eb61f07f238ca49c89fde
McKay Broderick <mckay.db@gmail.com>
2020-11-29 11:17:32 -0700

    Working on getting everything working again with the new naming

--- rust-arduino-01-blink/flash.sh
+++ rust-arduino-01-blink/flash.sh
@@ -4,6 +4,6 @@ set -e
 
 cargo build
 # avrdude -q -C/etc/avrdude.conf -patmega328p -carduino -P/dev/tty.usbmodem141201 -D "-Uflash:w:$1:e"
-avrdude -q -patmega328p -carduino -P/dev/tty.usbmodem144201 -D -Uflash:w:"target/avr-atmega328p/debug/rust-arduino-01-blink.elf"
+avrdude -q -patmega328p -carduino -P/dev/tty.usbmodem144301 -D -Uflash:w:"target/avr-atmega328p/debug/rust-arduino-01-blink.elf"
 
 
--- rust-arduino-03-love-o-meter/src/main.rs
+++ rust-arduino-03-love-o-meter/src/main.rs
@@ -1,34 +1,34 @@
-// main.rs
-
 #![no_std]
 #![no_main]
 
-extern crate panic_halt;
 use arduino_uno::prelude::*;
-use arduino_uno::hal::port::portb::PB5;
-use arduino_uno::hal::port::mode::Output;
+use panic_halt as _;
 
-
-fn stutter_blink(led: &mut PB5<Output>, times: usize) {
-    (0..times).map(|i| i * 10).for_each(|i| {
-        led.toggle().void_unwrap();
-        arduino_uno::delay_ms(i as u16);
-    });
-}
+// This example opens a serial connection to the host computer.  On most POSIX operating systems (like GNU/Linux or
+// OSX), you can interface with the program by running (assuming the device appears as ttyACM0)
+//
+// $ sudo screen /dev/ttyACM0 57600
 
 #[arduino_uno::entry]
 fn main() -> ! {
-    let peripherals = arduino_uno::Peripherals::take().unwrap();
+    let dp = arduino_uno::Peripherals::take().unwrap();
 
-    let mut pins = arduino_uno::Pins::new(
-        peripherals.PORTB,
-        peripherals.PORTC,
-        peripherals.PORTD,
+    let mut pins = arduino_uno::Pins::new(dp.PORTB, dp.PORTC, dp.PORTD);
+
+    let mut serial = arduino_uno::Serial::new(
+        dp.USART0,
+        pins.d0,
+        pins.d1.into_output(&mut pins.ddr),
+        9600.into_baudrate()
     );
 
-    let mut led = pins.d13.into_output(&mut pins.ddr);
+    ufmt::uwriteln!(&mut serial, "Hello from Arduino!\r").void_unwrap();
 
     loop {
-        stutter_blink(&mut led, 25);
+        // Read a byte from the serial connection
+        let b = nb::block!(serial.read()).void_unwrap();
+
+        // Answer
+        ufmt::uwriteln!(&mut serial, "Got {}!\r", b).void_unwrap();
     }
 }
