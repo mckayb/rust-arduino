f47cb6ae0423408341548d684f967572c37a7454
McKay Broderick <mckay.db@gmail.com>
2020-11-29 16:08:49 -0700

    Aggressively optimize to keep the build small. That seemed to work better when reading from Serial

--- rust-arduino-02-spaceship-interface/flash.sh
+++ rust-arduino-02-spaceship-interface/flash.sh
@@ -4,7 +4,7 @@ set -e
 
 cargo build
 # avrdude -q -C/etc/avrdude.conf -patmega328p -carduino -P/dev/tty.usbmodem141201 -D "-Uflash:w:$1:e"
-avrdude -q -patmega328p -carduino -P/dev/tty.usbmodem144201 -D -Uflash:w:"target/avr-atmega328p/debug/rust-arduino-02-spaceship-interface.elf"
+avrdude -q -patmega328p -carduino -P/dev/tty.usbmodem144301 -D -Uflash:w:"target/avr-atmega328p/debug/rust-arduino-02-spaceship-interface.elf"
 
 
 
