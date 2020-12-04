a9a69ba8400833446a919edd1df3ed2e4c1b8e9e
McKay Broderick <mckay.db@gmail.com>
2020-12-04 12:10:19 -0700

    Got the basics working... though I can't seem to figure out how to do floating point math

--- rust-arduino-03-love-o-meter/flash.sh
+++ rust-arduino-03-love-o-meter/flash.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-cargo build
+cargo build --release
 # avrdude -q -C/etc/avrdude.conf -patmega328p -carduino -P/dev/tty.usbmodem141201 -D "-Uflash:w:$1:e"
-avrdude -q -patmega328p -carduino -P/dev/tty.usbmodem144301 -D -Uflash:w:"target/avr-atmega328p/debug/rust-arduino-03-love-o-meter.elf"
+avrdude -q -patmega328p -carduino -P/dev/tty.usbmodem144301 -D -Uflash:w:"target/avr-atmega328p/release/rust-arduino-03-love-o-meter.elf"
 
 
--- rust-arduino-03-love-o-meter/src/main.rs
+++ rust-arduino-03-love-o-meter/src/main.rs
@@ -7,7 +7,7 @@ use panic_halt as _;
 // This example opens a serial connection to the host computer.  On most POSIX operating systems (like GNU/Linux or
 // OSX), you can interface with the program by running (assuming the device appears as ttyACM0)
 //
-// $ sudo screen /dev/ttyACM0 57600
+// $ sudo screen /dev/ttyACM0 9600
 
 #[arduino_uno::entry]
 fn main() -> ! {
@@ -19,16 +19,51 @@ fn main() -> ! {
         dp.USART0,
         pins.d0,
         pins.d1.into_output(&mut pins.ddr),
-        57600.into_baudrate(),
+        9600.into_baudrate(),
     );
 
-    ufmt::uwriteln!(&mut serial, "Hello from Arduino!\r").void_unwrap();
+    let mut adc = arduino_uno::adc::Adc::new(dp.ADC, Default::default());
+
+    let mut d2 = pins.d2.into_output(&mut pins.ddr);
+    let mut d3 = pins.d3.into_output(&mut pins.ddr);
+    let mut d4 = pins.d4.into_output(&mut pins.ddr);
+    let mut a0 = pins.a0.into_analog_input(&mut adc);
+
+    let baseline_temp: u16 = 20;
+
+    d2.set_low().void_unwrap();
+    d3.set_low().void_unwrap();
+    d4.set_low().void_unwrap();
 
     loop {
-        // Read a byte from the serial connection
-        let b = nb::block!(serial.read()).void_unwrap();
+        // This isn't perfect because for some reason, I can't get floating point math working properly...
+        let sensor_val: u16 = nb::block!(adc.read(&mut a0)).void_unwrap();
+        let voltage = ((sensor_val * 100) / 1024) * 5;
+        let temperature = voltage - 50;
 
-        // Answer
-        ufmt::uwriteln!(&mut serial, "Got {}!\r", b).void_unwrap();
+        // let sensor_val_str = sensor_val as u16;
+        ufmt::uwriteln!(&mut serial, "Sensor Value: {}\r", sensor_val as u16).void_unwrap();
+        ufmt::uwriteln!(&mut serial, "Volts (x100): {}\r", voltage as u16).void_unwrap();
+        ufmt::uwriteln!(&mut serial, "Degrees C: {}\r", temperature as u16).void_unwrap();
+
+        if temperature < baseline_temp {
+            d2.set_low().void_unwrap();
+            d3.set_low().void_unwrap();
+            d4.set_low().void_unwrap();
+        } else if temperature >= (baseline_temp + 2) && temperature < (baseline_temp + 4) {
+            d2.set_high().void_unwrap();
+            d3.set_low().void_unwrap();
+            d4.set_low().void_unwrap();
+        } else if temperature >= (baseline_temp + 4) && temperature < (baseline_temp + 6) {
+            d2.set_high().void_unwrap();
+            d3.set_high().void_unwrap();
+            d4.set_low().void_unwrap();
+        } else {
+            d2.set_high().void_unwrap();
+            d3.set_high().void_unwrap();
+            d4.set_high().void_unwrap();
+        }
+
+        arduino_uno::delay_ms(1000);
     }
 }
