9def622e34f20f762ae05a937613307dc27ae59a
McKay Broderick <mckay.db@gmail.com>
2020-12-05 12:40:26 -0700

    Did a little bit of clean up

--- rust-arduino-03-love-o-meter/src/main.rs
+++ rust-arduino-03-love-o-meter/src/main.rs
@@ -4,6 +4,7 @@
 use arduino_uno::prelude::*;
 use panic_halt as _;
 
+
 // This example opens a serial connection to the host computer.  On most POSIX operating systems (like GNU/Linux or
 // OSX), you can interface with the program by running (assuming the device appears as ttyACM0)
 //
@@ -29,32 +30,33 @@ fn main() -> ! {
     let mut d4 = pins.d4.into_output(&mut pins.ddr);
     let mut a0 = pins.a0.into_analog_input(&mut adc);
 
-    let baseline_temp: u16 = 20;
+    let baseline_temp: f32 = 20.0;
 
     d2.set_low().void_unwrap();
     d3.set_low().void_unwrap();
     d4.set_low().void_unwrap();
 
     loop {
-        // This isn't perfect because for some reason, I can't get floating point math working properly...
-        let sensor_val: u16 = nb::block!(adc.read(&mut a0)).void_unwrap();
-        let voltage = ((sensor_val * 100) / 1024) * 5;
-        let temperature = voltage - 50;
 
-        // let sensor_val_str = sensor_val as u16;
-        ufmt::uwriteln!(&mut serial, "Sensor Value: {}\r", sensor_val as u16).void_unwrap();
-        ufmt::uwriteln!(&mut serial, "Volts (x100): {}\r", voltage as u16).void_unwrap();
-        ufmt::uwriteln!(&mut serial, "Degrees C: {}\r", temperature as u16).void_unwrap();
+        let sensor_val: u16 = nb::block!(adc.read(&mut a0)).void_unwrap();
+        let voltage = ((sensor_val as f32) / 1024.0) * 5.0;
+        let temperature = (voltage - 0.5) * 100.0;
+
+        ufmt::uwriteln!(&mut serial, "Sensor Value: {}\r", sensor_val).void_unwrap();
+
+        // I would love to be able to do this... but f32 to string is not supported, and I can't find any way of getting it to work.
+        // ufmt::uwriteln!(&mut serial, "Volts (x100): {}\r", voltage).void_unwrap();
+        // ufmt::uwriteln!(&mut serial, "Degrees C: {}\r", temperature).void_unwrap();
 
         if temperature < baseline_temp {
             d2.set_low().void_unwrap();
             d3.set_low().void_unwrap();
             d4.set_low().void_unwrap();
-        } else if temperature >= (baseline_temp + 2) && temperature < (baseline_temp + 4) {
+        } else if temperature >= (baseline_temp + 2.0) && temperature < (baseline_temp + 4.0) {
             d2.set_high().void_unwrap();
             d3.set_low().void_unwrap();
             d4.set_low().void_unwrap();
-        } else if temperature >= (baseline_temp + 4) && temperature < (baseline_temp + 6) {
+        } else if temperature >= (baseline_temp + 4.0) && temperature < (baseline_temp + 6.0) {
             d2.set_high().void_unwrap();
             d3.set_high().void_unwrap();
             d4.set_low().void_unwrap();
